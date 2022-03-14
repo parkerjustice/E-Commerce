f58cc20057bdd850408aabdfb046d13850093ac6
parkerjustice <you@example.com>
2022-03-13 21:12:28 -0400

    added js

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  };
