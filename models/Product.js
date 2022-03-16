c5915fd42191a98b64bdf692becff0faef2527c5
parkerjustice <you@example.com>
2022-03-16 13:31:50 -0400

    added some js file changes

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull:false,
+    primaryKey: true,
+    autoIncrement: true
+  }
   },
+
+  pruduct_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  price: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+  }
+
   {
     sequelize,
     timestamps: false,
