7a78e19f7478ab579bb4d170926330aa91968224
parkerjustice <you@example.com>
2022-03-16 13:41:46 -0400

    fixed files

--- models/index.js
+++ models/index.js
@@ -2,6 +2,30 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+
+
+Product.belongsTo(Category, {
+  foreignKey: "catagory_id"
+
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
+//different than above
+//Allowing several tags to be available
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
+
+
 module.exports = {
     Product,
     Category,
